87208b8d3fc3cb5b8724fc55fc851c31fd7ed5d5
donpmusni <donpmusni@gmail.com>
2016-08-17 02:06:58 -0400

    Menu Changes
    Enqueued font awesome, appearance of header menu and hamburger menu on
    mobile.

--- functions.php
+++ functions.php
@@ -107,6 +107,9 @@ function assignmentone_scripts() {
 	wp_enqueue_script( 'assignmentone-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'assignmentone-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+/*enqueue font awesome */
+	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css' );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
